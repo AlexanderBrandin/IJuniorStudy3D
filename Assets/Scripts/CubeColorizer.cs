e4e33f40e3052b81fd8e9201aa0dc46d345b13e1
AlexanderBrandin <azartsb@yandex.ru>
2026-06-05 14:21:49 +0300

    Add cube rain object pool task
    Implement cube rain mechanic using a generic object pool.
    
    Cubes spawn above platforms at random positions and fall with Rigidbody gravity.
    All spawned cubes start with the same color.
    On first platform collision, each cube changes to a random color and starts a random lifetime timer.
    Expired cubes are returned to the pool instead of being destroyed.

--- Assets/Scripts/CubeColorizer.cs
+++ Assets/Scripts/CubeColorizer.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class CubeColorizer : MonoBehaviour
 {
-    public void ApplyRandomColor(ExplodableCube cube)
+    public void ApplyRandomColor(Renderer renderer)
     {
-        Renderer renderer = cube.GetComponent<Renderer>();
         renderer.material.color = Random.ColorHSV();
+    }
+
+    public void ApplyColor(Renderer renderer, Color color)
+    {
+        renderer.material.color = color;
     }
 }
--- Assets/Scripts/CubeSpawner.cs
+++ Assets/Scripts/CubeSpawner.cs
@@ -1,24 +1,75 @@
+using System.Collections;
 using UnityEngine;
 
 public class CubeSpawner : MonoBehaviour
 {
-    [SerializeField] private ExplodableCube _cubePrefab;
+    [SerializeField] private FallingCube _cubePrefab;
+    [SerializeField] private CubeColorizer _colorizer;
+    [SerializeField] private Transform _spawnCenter;
+    [SerializeField] private Transform _poolContainer;
 
-    public ExplodableCube Spawn(Vector3 position, Quaternion rotation, Vector3 scale, float splitChance)
+    [SerializeField] private int _initialPoolSize;
+    [SerializeField] private float _spawnDelay;
+    [SerializeField] private Vector2 _spawnAreaSize;
+    [SerializeField] private Color _startColor;
+
+    private ObjectPool<FallingCube> _pool;
+    private Coroutine _spawnCoroutine;
+
+    private void Awake()
     {
-        ExplodableCube cube = Instantiate(_cubePrefab, position, rotation);
-
-        cube.transform.localScale = scale;
-        cube.Initialize(splitChance);
-
-        return cube;
+        _pool = new ObjectPool<FallingCube>(_cubePrefab, _initialPoolSize, _poolContainer);
     }
 
-    public void Despawn(ExplodableCube cube)
+    private void OnEnable()
     {
-        if (cube == null)
-            return;
+        _spawnCoroutine = StartCoroutine(SpawnCubes());
+    }
 
-        Destroy(cube.gameObject);
+    private void OnDisable()
+    {
+        if (_spawnCoroutine != null)
+            StopCoroutine(_spawnCoroutine);
+    }
+
+    private IEnumerator SpawnCubes()
+    {
+        WaitForSeconds wait = new WaitForSeconds(_spawnDelay);
+
+        while (enabled)
+        {
+            SpawnCube();
+
+            yield return wait;
+        }
+    }
+
+    private void SpawnCube()
+    {
+        FallingCube cube = _pool.Get();
+
+        cube.LifeTimeExpired += ReleaseCube;
+        cube.Initialize(_colorizer, _startColor);
+
+        cube.transform.position = GetRandomSpawnPosition();
+        cube.transform.rotation = Random.rotation;
+    }
+
+    private void ReleaseCube(FallingCube cube)
+    {
+        cube.LifeTimeExpired -= ReleaseCube;
+
+        _pool.Release(cube);
+    }
+
+    private Vector3 GetRandomSpawnPosition()
+    {
+        float halfWidth = _spawnAreaSize.x / 2f;
+        float halfDepth = _spawnAreaSize.y / 2f;
+
+        float x = Random.Range(-halfWidth, halfWidth);
+        float z = Random.Range(-halfDepth, halfDepth);
+
+        return _spawnCenter.position + new Vector3(x, 0f, z);
     }
 }
